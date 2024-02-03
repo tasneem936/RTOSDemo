7481a7a19b25fa923cde72eaec98e2dc95dd2334
tasneem <tasneem.mansour36@gmail.com>
2024-02-03 22:28:15 +0200

    Add project files.

---
+++ Examples/Example001/main.c
@@ -0,0 +1,89 @@
+/* FreeRTOS.org includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+
+/* Demo includes. */
+#include "supporting_functions.h"
+
+/* Used as a loop counter to create a very crude delay. */
+#define mainDELAY_LOOP_COUNT		( 0xffffffff)
+
+/* The task functions. */
+void vTask1( void *pvParameters );
+void vTask2( void *pvParameters );
+
+/*-----------------------------------------------------------*/
+
+int main( void )
+{
+	/* Create one of the two tasks. */
+	xTaskCreate(	vTask1,		/* Pointer to the function that implements the task. */
+					"Task 1",	/* Text name for the task.  This is to facilitate debugging only. */
+					1000,		/* Stack depth - most small microcontrollers will use much less stack than this. */
+					NULL,		/* We are not using the task parameter. */
+					1,			/* This task will run at priority 1. */
+					NULL );		/* We are not using the task handle. */
+
+	/* Create the other task in exactly the same way. */
+	xTaskCreate( vTask2, "Task 2", 1000, NULL, 1, NULL );
+
+	/* Start the scheduler to start the tasks executing. */
+	vTaskStartScheduler();	
+
+	/* The following line should never be reached because vTaskStartScheduler() 
+	will only return if there was not enough FreeRTOS heap memory available to
+	create the Idle and (if configured) Timer tasks.  Heap management, and
+	techniques for trapping heap exhaustion, are described in the book text. */
+	for( ;; );
+	return 0;
+}
+/*-----------------------------------------------------------*/
+
+void vTask1( void *pvParameters )
+{
+const char *pcTaskName = "Task 1 is running\r\n";
+const char* another_string = "done 1 ****\r\n";
+volatile uint32_t ul;
+
+	/* As per most tasks, this task is implemented in an infinite loop. */
+	for( ;; )
+	{
+		/* Print out the name of this task. */
+		vPrintString( pcTaskName );
+
+		/* Delay for a period. */
+		for( ul = 0; ul < mainDELAY_LOOP_COUNT; ul++ )
+		{
+			/* This loop is just a very crude delay implementation.  There is
+			nothing to do in here.  Later exercises will replace this crude
+			loop with a proper delay/sleep function. */
+		}
+		vPrintString(another_string);
+	}
+}
+/*-----------------------------------------------------------*/
+
+void vTask2( void *pvParameters )
+{
+const char *pcTaskName = "Task 2 is running\r\n";
+const char* another_string = "done 2  ####\r\n";
+volatile uint32_t ul;
+
+	/* As per most tasks, this task is implemented in an infinite loop. */
+	for( ;; )
+	{
+		/* Print out the name of this task. */
+		vPrintString( pcTaskName );
+
+		/* Delay for a period. */
+		for( ul = 0; ul < mainDELAY_LOOP_COUNT; ul++ )
+		{
+			/* This loop is just a very crude delay implementation.  There is
+			nothing to do in here.  Later exercises will replace this crude
+			loop with a proper delay/sleep function. */
+		}
+		vPrintString(another_string);
+	}
+}
+
+
---
+++ Examples/Example004/main.c
@@ -0,0 +1,69 @@
+
+
+/* FreeRTOS.org includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+
+/* Demo includes. */
+#include "supporting_functions.h"
+
+/* The task function. */
+void vTaskFunction( void *pvParameters );
+
+/* Define the strings that will be passed in as the task parameters.  These are
+defined const and off the stack to ensure they remain valid when the tasks are
+executing. */
+const char *pcTextForTask1 = "Task 1 is running\r\n";
+const char *pcTextForTask2 = "Task 2 is running\r\n";
+#define mainDELAY_LOOP_COUNT		( 0xfffffff )
+
+/*-----------------------------------------------------------*/
+
+int main( void )
+{
+	/* Create the first task at priority 1... */
+	xTaskCreate( vTaskFunction, "Task 1", 1000, (void*)pcTextForTask1, 1, NULL );
+
+	/* ... and the second task at priority 2.  The priority is the second to
+	last parameter. */
+	xTaskCreate( vTaskFunction, "Task 2", 1000, (void*)pcTextForTask2, 2, NULL );
+
+	/* Start the scheduler to start the tasks executing. */
+	vTaskStartScheduler();
+
+	/* The following line should never be reached because vTaskStartScheduler()
+	will only return if there was not enough FreeRTOS heap memory available to
+	create the Idle and (if configured) Timer tasks.  Heap management, and
+	techniques for trapping heap exhaustion, are described in the book text. */
+	for( ;; );
+	return 0;
+}
+/*-----------------------------------------------------------*/
+
+void vTaskFunction( void *pvParameters )
+{
+char *pcTaskName;
+const TickType_t xDelay250ms = pdMS_TO_TICKS( 250UL );
+
+	/* The string to print out is passed in via the parameter.  Cast this to a
+	character pointer. */
+	pcTaskName = ( char * ) pvParameters;
+	volatile uint32_t ul;
+	/* As per most tasks, this task is implemented in an infinite loop. */
+	for( ;; )
+	{
+		/* Print out the name of this task. */
+		vPrintString( pcTaskName );
+
+		/* Delay for a period.  This time a call to vTaskDelay() is used which
+		places the task into the Blocked state until the delay period has
+		expired.  The parameter takes a time specified in 'ticks', and the
+		pdMS_TO_TICKS() macro is used (where the xDelay250ms constant is
+		declared) to convert 250 milliseconds into an equivalent time in
+		ticks. */
+		//for (ul = 0; ul < mainDELAY_LOOP_COUNT; ul++);
+		vTaskDelay( xDelay250ms );
+	}
+}
+
+
